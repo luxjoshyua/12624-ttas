4e370786e7279525886cc934b4a444020908f90f
Michelle Watts <michelle.watts@nine.com.au>
2023-10-30 14:34:00 +1100

    the typescript main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.js'
-import './styles/main.scss'
-import './js/masthead/index.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "./styles/main.scss";
+import "./js/masthead/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
