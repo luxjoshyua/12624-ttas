6b21499cac16e29cd93c10ed23d636d764ce5071
Michelle Watts <michelle.watts@nine.com.au>
2023-12-08 09:30:28 +1100

    boilerplate updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,18 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended", // Add TypeScript rules
   ],
+  parser: "@typescript-eslint/parser", // Specify the TypeScript parser
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: [
+    "react-refresh",
+    "@typescript-eslint" // Add TypeScript plugin
+  ],
   rules: {
     "react-refresh/only-export-components": "warn",
+    // Add or override TypeScript-specific rules here
   },
 };
+
