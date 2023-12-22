4e530d22955820666bdb1aad247c4add30df2f2b
Michelle Watts <michelle.watts@nine.com.au>
2023-12-22 12:18:45 +1100

    brightcove video component

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -9,28 +9,29 @@ declare global {
   }
 }
 
-const Video: React.FC = () => {
-  useExternalScripts(
-    "https://players.brightcove.net/6165065566001/wIpGq2Kd0p_default/index.min.js"
-  );
+interface VideoProps {
+  scriptUrl: string;
+  playerId: string;
+  videoId: string;
+}
+
+const Video: React.FC<VideoProps> = ({ scriptUrl, playerId, videoId }) => {
+  useExternalScripts(scriptUrl);
 
   return (
     <>
-      <div className="full-bg z-[-1]">
-        <video-js
-          id="bg-video"
-          data-account="6165065566001"
-          data-player="wIpGq2Kd0p"
-          data-video-id="6338956021112"
-          data-embed="default"
-          data-application-id
-          autoPlay
-          loop
-          muted
-          controls
-          style={{ objectFit: "cover" }}
-        />
-      </div>
+      <video-js
+        data-account="6165065566001"
+        data-player={playerId}
+        data-embed="default"
+        controls=""
+        data-video-id={videoId}
+        data-playlist-id=""
+        data-application-id=""
+        class="vjs-fluid"
+        loop
+      ></video-js>
+
       <h3 className="reader-only">Video</h3>
     </>
   );
