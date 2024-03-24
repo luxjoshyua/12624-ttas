52b97bb3b340e324e65b115825e0c5790bd116f5
luxjoshyua <josh.e.fielding@gmail.com>
2024-03-25 10:56:51 +1100

    metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import reactRefresh from "@vitejs/plugin-react-refresh";
-import viteImagemin from 'vite-plugin-imagemin';
-import tsconfigPaths from 'vite-tsconfig-paths'
+import viteImagemin from "vite-plugin-imagemin";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,33 +10,33 @@ export default defineConfig({
   plugins: [
     react(),
     reactRefresh(),
-	tsconfigPaths(),
+    tsconfigPaths(),
     viteImagemin({
-			gifsicle: {
-				optimizationLevel: 7,
-				interlaced: false
-			},
-			optipng: {
-				optimizationLevel: 7
-			},
-			mozjpeg: {
-				quality: 70
-			},
-			pngquant: {
-				quality: [0.8, 0.9],
-				speed: 4
-			},
-			svgo: {
-				plugins: [
-					{
-						name: 'removeViewBox'
-					},
-					{
-						name: 'removeEmptyAttrs',
-						active: false
-					}
-				]
-			}
-		})
-  ]
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 70,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
 });
