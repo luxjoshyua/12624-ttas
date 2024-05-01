3b54dcb4d25c704be5c8b167e1b3ab7d4606156f
luxjoshyua <josh.e.fielding@gmail.com>
2024-05-02 09:52:10 +1000

    removed unused layout effect

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,12 @@ import Hero from "./components/blocks/Hero/Hero.tsx";
 import SectionTwo from "./components/blocks/SectionTwo/index.tsx";
 import SectionThree from "./components/blocks/SectionThree/index.tsx";
 import Header from "./components/Header";
-// import CustomLink from "./components/blocks/CustomLink";
 
 import LocomotiveParallax from "./js/parallax/index.tsx";
 
 const App = () => {
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
   const locoScrollRef = useRef(null);
-
-  // useLayoutEffect(() => {
-  //   ScrollSmoother.create({
-  //     smooth: 1,
-  //     effects: true,
-  //     speed: 0.8,
-  //   });
-  // }, []);
-
-  useLayoutEffect(() => {
-    const headings = document.querySelectorAll(".scroll-trigger");
-    headings.forEach((heading) => {
-      let ctx = gsap.context(() => {
-        gsap.fromTo(
-          heading,
-          { opacity: 0 },
-          {
-            opacity: 1,
-            duration: 2,
-            scrollTrigger: {
-              trigger: heading,
-              scrub: 1,
-            },
-          }
-        );
-      }, heading);
-      return () => ctx.revert();
-    });
-  }, []);
 
   return (
     <>
